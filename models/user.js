5efdf934b36b0d7a14693d335e95b2fc36bfd122
Shahzaib Rind <shahzaibrind6@outlook.com>
2021-09-09 01:06:22 +0500

    implemeted expiry feature for login

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
 
 
 userSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign({ _id: this._id}, config.get('jwtPrivateKey'));
+  const token = jwt.sign({ _id: this._id}, config.get('jwtPrivateKey'), { expiresIn: 900 });
   return token;
 }
 
